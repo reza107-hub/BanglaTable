9baf6a5ed2841820a3a531facd61fb11509a9216
reza107-hub <rashidaminur104@gmail.com>
2023-05-02 00:46:07 +0600

    footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="mt-2">
       <Navbar></Navbar>
-      <Home />
+      <div className="min-h-[calc(100vh-66px)]">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
 
